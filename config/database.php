0e80e189dbf468d633dc8f1797fce7bb78084506
wenshan <hou_ve@hotmail.com>
2024-06-09 14:53:22 +0800

    fix: init data

--- config/database.php
+++ config/database.php
@@ -4,22 +4,21 @@
  * 如果修改为mysql数据库，请同时修改type和dbname两个参数
  */
 return array(
-
-    'database' => array(
-
-        'type' => 'sqlite', // 数据库连接驱动类型: mysqli,sqlite,pdo_mysql,pdo_sqlite
-
-        'host' => '127.0.0.1', // 数据库服务器
-
-        'user' => 'pboot', // 数据库连接用户名
-
-        'passwd' => '123456', // 数据库连接密码
-
-        'port' => '3306', // 数据库端口
-
-        //'dbname' => 'pbootcms' // 去掉注释，启用mysql数据库，注意修改前面的连接信息及type为mysqli
-
-        'dbname' => '/data/isbestMachine.db' // 去掉注释，启用Sqlite数据库，注意修改type为sqlite
-    )
-
+    
+  'database' => array(
+      
+      'type' => 'mysqli', // 数据库连接驱动类型: mysqli,sqlite,pdo_mysql,pdo_sqlite
+      
+      'host' => '127.0.0.1', // 数据库服务器
+      
+      'user' => 'isbestMachine', // 数据库连接用户名
+      
+      'passwd' => 'JGW7GEFephXi72ba', // 数据库连接密码
+      
+      'port' => '3306', // 数据库端口
+                        
+      'dbname' => 'isbestmachine' // 去掉注释，启用mysql数据库，注意修改前面的连接信息及type为mysqli
+      
+      // 'dbname' => '/data/aa41ff0dc66de644a1c27a38048d1c67.db' // 去掉注释，启用Sqlite数据库，注意修改type为sqlite
+  )
 );
